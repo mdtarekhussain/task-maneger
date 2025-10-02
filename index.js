d940adead808c0396a00086e045d8e1c8cfc6f0a
Tarek Hossain <tarekhussainmd02@gmail.com>
2025-10-02 18:45:58 +0600

    remove listen

--- index.js
+++ index.js
@@ -138,6 +138,6 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
