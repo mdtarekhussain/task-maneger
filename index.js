d7808d0c06ed81d52c8d6bf3eaeb0194e5759e38
Tarek Hossain <tarekhussainmd02@gmail.com>
2025-10-02 21:37:13 +0600

    akd

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ app.get("/", (req, res) => {
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
-// module.exports = app;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
